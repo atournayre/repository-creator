408cf311094fe638dfb7e7cd51c25fbeef0797c7
atournayre <a.tournayre@aroban.com>
2023-06-03 15:07:00 +0200

    Refactor InitCommand to download files and issues templates from repositories

--- src/Command/InitCommand.php
+++ src/Command/InitCommand.php
@@ -12,18 +12,11 @@ use Webmozart\Assert\Assert;
 
 class InitCommand extends Command
 {
-    private readonly SymfonyStyle $io;
-    private readonly Filesystem $filesystem;
-
-    protected function initialize(InputInterface $input, OutputInterface $output): void
-    {
-        parent::initialize($input, $output);
-        $this->io = new SymfonyStyle($input, $output);
-        $this->filesystem = new Filesystem();
-    }
-
     private const DEFAULT_ISSUES_REPOSITORY = 'atournayre/create-repository-issues-template';
     private const DEFAULT_FILES_REPOSITORY = 'atournayre/create-repository-files-template';
+
+    private readonly SymfonyStyle $io;
+    private readonly Filesystem $filesystem;
 
     /**
      * @throws \Exception
@@ -34,21 +27,13 @@ class InitCommand extends Command
         Assert::endsWith($configFilePath, '.yaml', 'The configuration file must be a YAML file (.yaml).');
 
         $this->createConfigFile($configFilePath);
-        $this->initializeIssuesFolder($input->getArgument('issuesRepository'));
-        $this->initializeFilesFolder($input->getArgument('filesRepository'));
+
+        $currentPath = realpath(dirname($configFilePath));
+
+        $this->initializeIssuesFolder($currentPath, $input->getArgument('issuesRepository'));
+        $this->initializeFilesFolder($currentPath, $input->getArgument('filesRepository'));
 
         return Command::SUCCESS;
-    }
-
-    protected function configure(): void
-    {
-        $this->setName('init')
-            ->setDescription('Initializes the create repository command')
-            ->setHelp('This command initializes the create repository command')
-            ->addArgument('path', null, InputArgument::REQUIRED, 'Path to the configuration file')
-            ->addArgument('issuesRepository', InputArgument::OPTIONAL, 'GitHub repository name for issues', self::DEFAULT_ISSUES_REPOSITORY)
-            ->addArgument('filesRepository', InputArgument::OPTIONAL, 'GitHub repository name for files', self::DEFAULT_FILES_REPOSITORY)
-        ;
     }
 
     private function createConfigFile(string $configFilePath): void
@@ -63,15 +48,74 @@ class InitCommand extends Command
         $this->io->error(sprintf('Configuration file could not be created at %s', $configFilePath));
     }
 
-    private function initializeIssuesFolder(mixed $getArgument)
+    private function initializeIssuesFolder(string $currentPath, string $repositoryName): void
     {
-        $this->filesystem->mkdir('issues');
-        throw new \Exception('Not implemented yet');
+        $this->initializeFolder($currentPath, 'issues', $repositoryName);
     }
 
-    private function initializeFilesFolder(mixed $getArgument)
+    private function initializeFolder(string $currentPath, string $folder, string $repositoryName): void
     {
-        $this->filesystem->mkdir('files');
-        throw new \Exception('Not implemented yet');
+        $zipName = sprintf('%s/%s-master.zip', $currentPath, $folder);
+        $this->downloadZip($zipName, $repositoryName);
+        $this->extractZip($zipName, $currentPath . \DIRECTORY_SEPARATOR);
+        $this->filesystem->rename(
+            explode(\DIRECTORY_SEPARATOR, $repositoryName)[1] . '-master',
+            $currentPath . \DIRECTORY_SEPARATOR . $folder,
+            true
+        );
+        $this->filesystem->remove($zipName);
+    }
+
+    private function downloadZip(string $zipName, string $repositoryName): void
+    {
+        $this->filesystem->copy($this->buildZipUrl($repositoryName), $zipName);
+    }
+
+    private function buildZipUrl(string $repositoryName): string
+    {
+        return sprintf('https://github.com/%s/archive/refs/heads/master.zip', $repositoryName);
+    }
+
+    /**
+     * @param string $zipName
+     * @param string $destination
+     * @return void
+     */
+    protected function extractZip(string $zipName, string $destination): void
+    {
+        if (!extension_loaded('zip')) {
+            throw new \RuntimeException('The zip extension must be loaded to use this command.');
+        }
+
+        if (!$this->filesystem->exists($zipName)) {
+            throw new \RuntimeException(sprintf('The zip file %s does not exist.', $zipName));
+        }
+
+        $zipArchive = new \ZipArchive();
+        $zipArchive->open($zipName);
+        $zipArchive->extractTo($destination);
+        $zipArchive->close();
+    }
+
+    private function initializeFilesFolder(string $currentPath, string $repositoryName): void
+    {
+        $this->initializeFolder($currentPath, 'files', $repositoryName);
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output): void
+    {
+        parent::initialize($input, $output);
+        $this->io = new SymfonyStyle($input, $output);
+        $this->filesystem = new Filesystem();
+    }
+
+    protected function configure(): void
+    {
+        $this->setName('init')
+            ->setDescription('Initializes the create repository command')
+            ->setHelp('This command initializes the create repository command')
+            ->addArgument('path', null, InputArgument::REQUIRED, 'Path to the configuration file')
+            ->addArgument('issuesRepository', InputArgument::OPTIONAL, 'GitHub repository name for issues', self::DEFAULT_ISSUES_REPOSITORY)
+            ->addArgument('filesRepository', InputArgument::OPTIONAL, 'GitHub repository name for files', self::DEFAULT_FILES_REPOSITORY);
     }
 }
